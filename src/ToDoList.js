e4712d4d5edff3c3e72cc53834eb4180a415d4e0
Yara-AlH <139350596+Yara-AlH@users.noreply.github.com>
2024-01-29 19:03:27 +0100

    clean up

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -15,12 +15,6 @@ function ToDoList() {
       <h1 className="text-center m-5 mb-1">Quest Queue</h1>
       <div className="subhead mb-5 ">A To-Do list tracker</div>
       <Box />
-      {/* <footer className="github-link">
-        Made with 🤍 by{" "}
-        <a href="https://github.com/Yara-AlH" target="_blank" rel="noreferrer">
-          Yara
-        </a>
-      </footer> */}
     </div>
   );
 }
